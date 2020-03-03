cdbb12172ba1cc0fd593281320fd1298e88269ef
pizzacoca <pizzacoca@aquilenet.fr>
2020-03-03 13:34:49 +0100

    sauvegarde locale ajoutée

--- scripts/copie.sh
+++ scripts/copie.sh
@@ -7,6 +7,11 @@
 #
 
 
+echo "cp -r  ~/Bureau/espaces/www/flask-scripts/*" 
+echo "cp -r  ~/Bureau/espaces/www/flask-scripts/*" | bash
+echo "rm -rf  ~/Bureau/espaces/www/flask-scripts.bak" 
 echo "rm -rf  ~/Bureau/espaces/www/flask-scripts/*" | bash
+echo "cp -r ../flask/flask-scripts ~/Bureau/espaces/www" 
 echo "cp -r ../flask/flask-scripts ~/Bureau/espaces/www" | bash
+echo "ls -al ~/Bureau/espaces/www/flask-scripts" 
 echo "ls -al ~/Bureau/espaces/www/flask-scripts" | bash
