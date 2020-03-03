89ed08a366541e06b69d0ba4c1bb74e9286c9b8f
pizzacoca <pizzacoca@aquilenet.fr>
2020-03-03 10:03:04 +0100

    modif script maj

--- scripts/maj-site.sh
+++ scripts/maj-site.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp  -r /home/florian/git/hotel/flask/flask-scripts fpizzacoca@$1:/net/cremi/fpizzacoca/Bureau/espaces/www
+scp  -r ../* hbelaribi@$1:/net/cremi/hbelaribi/Bureau/espaces/www/flask-scripts
