3ff5d76a3293efd0e1a3f47238c1d4898b743974
habsinn <habsinn@habsinn.habsinn.org>
2020-03-03 09:55:11 +0100

    updated templates/

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+#où $1 est l'argument à passer lorsqu'on appelle le script. L'argument est le serveur distant sur lequel on se connecte en SSH (exemple: 'distant_escher' sur la configuraiton de ma machine tel que renseignée dans mon .ssh/config )
 echo "ouverture du port local 127.0.0.1:33333 vers $1:5000"
 echo "ssh -f hbelaribi@$1 -L 33333:localhost:5000 -N"
 ssh -A -f hbelaribi@$1 -L 33333:localhost:5000 -N
